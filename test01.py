2da7b03480edc816df4be33985f5aa55f79e84c7
MiraGlez <137509907+MiraGlez@users.noreply.github.com>
2023-06-23 16:15:27 +0200

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.write("Hola Miri")
+
+df = pd.read_csv("ACPs_Breast_cancer.csv")
+
+print(df.isnull())
+print(df.isnull().sum())
+
+print (df)
+
+
