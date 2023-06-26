eab3a8ec7f8af09d753f89a8a5f5bfa49d6f564b
MiraGlez <137509907+MiraGlez@users.noreply.github.com>
2023-06-26 11:12:14 +0200

    Update test01.py

--- test01.py
+++ test01.py
@@ -3,17 +3,12 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="TESTING PROJECT", page_icon=":bar_chart:", layout="wide")
+#st.set_page_config(page_title="TESTING PROJECT", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 
-df = pd.read_excel(
-        io="wdbc_mfg.xlsx",
-        engine="openpyxl",
-        sheet_name="wdbc",
-        #skiprows=3,
-        usecols="A:L",
-        nrows=569)
+df = pd.read_excel("wdbc_mfg.xlsx",sheet_name="wdbc")
 
 st.dataframe(df)
+
 
