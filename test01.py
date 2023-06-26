17bd91b6df13cf860c1224b392156a866eae1091
MiraGlez <137509907+MiraGlez@users.noreply.github.com>
2023-06-26 10:52:14 +0200

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,14 +1,19 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
 
-st.write("Hola Miri")
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="TESTING PROJECT", page_icon=":bar_chart:", layout="wide")
 
-df = pd.read_csv("ACPs_Breast_cancer.csv")
+# ---- READ EXCEL ----
 
-print(df.isnull())
-print(df.isnull().sum())
+df = pd.read_excel(
+        io="wdbc_mfg.xlsx",
+        engine="openpyxl",
+        sheet_name="wdbc",
+        #skiprows=3,
+        usecols="A:L",
+        nrows=569)
 
-print (df)
-
+st.dataframe(df)
 
